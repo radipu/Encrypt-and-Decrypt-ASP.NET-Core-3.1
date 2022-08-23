9753d6410dbdbbfb11c38f0ef83aa4cc65f3c82a
radipu <rad.edu.du@gmail.com>
2022-08-23 11:58:06 +0600

    solution

---
+++ EncryptAndDecrypt/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace EncryptAndDecrypt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = "Hello!";
+            var encryptedText = EncryptPlainTextToCipherText(text);
+            var decryptedText = DecryptCipherTextToPlainText(encryptedText);
+
+
+            Console.WriteLine("Passed Text = " + text);
+            Console.WriteLine("EncryptedText = " + encryptedText);
+            Console.WriteLine("DecryptedText = " + decryptedText);
+            Console.ReadLine();
+        }
+
+        private const string SecurityKey = "ComplexKeyHere";
+
+        public static string EncryptPlainTextToCipherText(string PlainText)
+        {
+            byte[] toEncryptedArray = UTF8Encoding.UTF8.GetBytes(PlainText);
+
+            MD5CryptoServiceProvider objMD5CryptoService = new MD5CryptoServiceProvider();
+            byte[] securityKeyArray = objMD5CryptoService.ComputeHash(UTF8Encoding.UTF8.GetBytes(SecurityKey));
+            objMD5CryptoService.Clear();
+
+            var objTripleDESCryptoService = new TripleDESCryptoServiceProvider();
+            objTripleDESCryptoService.Key = securityKeyArray;
+            objTripleDESCryptoService.Mode = CipherMode.ECB;
+            objTripleDESCryptoService.Padding = PaddingMode.PKCS7;
+
+
+            var objCrytpoTransform = objTripleDESCryptoService.CreateEncryptor();
+            byte[] resultArray = objCrytpoTransform.TransformFinalBlock(toEncryptedArray, 0, toEncryptedArray.Length);
+            objTripleDESCryptoService.Clear();
+            return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+        }
+
+        public static string DecryptCipherTextToPlainText(string CipherText)
+        {
+            byte[] toEncryptArray = Convert.FromBase64String(CipherText);
+            MD5CryptoServiceProvider objMD5CryptoService = new MD5CryptoServiceProvider();
+
+            byte[] securityKeyArray = objMD5CryptoService.ComputeHash(UTF8Encoding.UTF8.GetBytes(SecurityKey));
+            objMD5CryptoService.Clear();
+
+            var objTripleDESCryptoService = new TripleDESCryptoServiceProvider();
+            objTripleDESCryptoService.Key = securityKeyArray;
+            objTripleDESCryptoService.Mode = CipherMode.ECB;
+            objTripleDESCryptoService.Padding = PaddingMode.PKCS7;
+
+            var objCrytpoTransform = objTripleDESCryptoService.CreateDecryptor();
+            byte[] resultArray = objCrytpoTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+            objTripleDESCryptoService.Clear();
+
+            return UTF8Encoding.UTF8.GetString(resultArray);
+        }
+    }
+}
